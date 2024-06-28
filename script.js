98e0ba2a30fb201a4b41d98df96efb1ac245e7ca
Crehan <crehanas14@gmail.com>
2024-06-28 13:44:10 +0530

    added scroll event listener

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 let i = 0;
 const name = "^1000 I'm Crehan Santhumayor";
-
-document.querySelector("body").onload = () => {};
-
 const animated = document.querySelector(".fadeInUp-animation");
+let firstScroll = false;
+
+const handleFirstScroll = (event) => {
+  if (!firstScroll) {
+    firstScroll = true;
+  } else {
+    const navbar = document.getElementById("navbar");
+    const aTags = navbar.getElementsByTagName("a");
+    for (var i = 0; i < aTags.length; i++) {
+      aTags[i].style.color = "white";
+    }
+    document.removeEventListener("scroll", handleFirstScroll);
+    const downArrowElement = document.querySelector(".down-arrow");
+    downArrowElement.classList.toggle("black2gray");
+    downArrowElement.classList.toggle("gray2black");
+  }
+};
 
 animated.addEventListener("animationend", () => {
   console.log("Animation ended");
@@ -12,14 +26,7 @@ animated.addEventListener("animationend", () => {
     typeSpeed: 50,
     onComplete: () => {
       document.querySelector(".down-arrow").classList.toggle("black2gray");
+      document.addEventListener("scroll", handleFirstScroll);
     },
   });
 });
-
-window.onscroll = function () {
-  scrollFunction();
-};
-
-function scrollFunction() {
-  console.log("hello");
-}
