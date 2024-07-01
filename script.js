3332d0bcb6440ffbdaec86e3f27044b3d7baf93e
Crehan <crehanas14@gmail.com>
2024-07-02 01:04:29 +0530

    fixed layout and add content

--- script.js
+++ script.js
@@ -3,6 +3,21 @@ const name = "^1000 I'm \n Crehan \n Santhumayor";
 const animated = document.querySelector(".fadeInUp-animation");
 let firstScroll = false;
 
+// The function actually applying the offset
+function offsetAnchor() {
+  if (location.hash.length !== 0) {
+    window.scrollTo(window.scrollX, window.scrollY - 100);
+  }
+}
+
+// This will capture hash changes while on the page
+window.addEventListener("hashchange", offsetAnchor);
+
+// This is here so that when you enter the page with a hash,
+// it can provide the offset in that case too. Having a timeout
+// seems necessary to allow the browser to jump to the anchor first.
+window.setTimeout(offsetAnchor, 1); // The delay of 1 is arbitrary and may not always work right (although it did in my testing).
+
 const handleFirstScroll = (event) => {
   if (!firstScroll) {
     firstScroll = true;
